3613bb785122b007fca4ae4199eb57e4e2541833
heojinho <hjh701234@gmail.com>
2021-10-08 12:53:16 +0900

    last Commit@!!

--- src/test/java/com/freelec/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/freelec/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
@@ -2,20 +2,15 @@ package com.freelec.freelecspringboot2webservice.domain.posts;
 
 
 import org.assertj.core.api.Assertions;
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
