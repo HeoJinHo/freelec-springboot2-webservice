aea8b6b14c7c2ea8792673feccc6c17a1edb9222
heojinho <hjh701234@gmail.com>
2021-10-08 12:42:37 +0900

    last Commit@!!

--- src/main/java/com/freelec/freelecspringboot2webservice/web/IndexController.java
+++ src/main/java/com/freelec/freelecspringboot2webservice/web/IndexController.java
@@ -4,14 +4,12 @@ import com.freelec.freelecspringboot2webservice.config.auth.LoginUser;
 import com.freelec.freelecspringboot2webservice.config.auth.SessionUser;
 import com.freelec.freelecspringboot2webservice.service.posts.PostsService;
 import com.freelec.freelecspringboot2webservice.web.dto.PostsResponseDto;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
